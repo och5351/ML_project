3062e15ff38d902c5ef9a297842568f5dee6f1be
백정훈 <bjh7013@gmail.com>
2021-01-23 10:15:32 +0900

    complete

---
+++ Jeonghoon/Data_Year.java
@@ -0,0 +1,223 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Data_Year {
+	public static void main(String[] args) throws IOException {
+		List<List<String>> ret = new ArrayList<List<String>>();
+		BufferedReader br = null;
+		BufferedReader br_join = null;
+		BufferedWriter brwrite = null;
+		try {
+			br = Files.newBufferedReader(Paths.get("C:\\Users\\Baek\\Desktop\\SK데이터분석\\맥북\\train.csv"));
+			br_join = Files.newBufferedReader(Paths.get("C:\\Users\\Baek\\Desktop\\SK데이터분석\\맥북\\train_display_real.csv"));
+			brwrite = Files.newBufferedWriter(Paths.get("C:\\Users\\Baek\\Desktop\\SK데이터분석\\맥북\\complete.csv"));
+			String line = "";
+			String line_join = "";
+			while ((line = br.readLine()) != null && (line_join = br_join.readLine()) != null) {
+				System.out.println(line);
+				System.out.println(line_join);
+				// CSV 1행을 저장하는 리스트
+				List<String> tmpList = new ArrayList<String>();
+				List<String> tmpList_join = new ArrayList<String>();
+				line = line.replaceAll("\"", "");
+				String array[] = line.split(",");
+				String array_join[] = line_join.split(",");
+				String temp = array[1];
+				array[1] = array[1].toUpperCase(); // 임시로 전부 대문자로 변환
+				int year = array.length - 4;
+				for (int i = 0; i < array.length; i++) {
+					if (i == 1) {
+						// 제목에서의 연도에 따라 자동매핑
+
+						if (array[1].contains("2009")) {
+							array[year] = "2009";
+						} else if (array[1].contains("2010")) {
+							array[year] = "2010";
+						} else if (array[1].contains("2011")) {
+							array[year] = "2011";
+						} else if (array[1].contains("2012")) {
+							array[year] = "2012";
+						} else if (array[1].contains("2013")) {
+							array[year] = "2013";
+						} else if (array[1].contains("2014")) {
+							array[year] = "2014";
+						} else if (array[1].contains("2015")) {
+							array[year] = "2015";
+						} else if (array[1].contains("2016")) {
+							array[year] = "2016";
+						} else if (array[1].contains("2017")) {
+							array[year] = "2017";
+						} else if (array[1].contains("2018")) {
+							array[year] = "2018";
+						} else if (array[1].contains("2019")) {
+							array[year] = "2019";
+						} else if (array[1].contains("2020")) {
+							array[year] = "2020";
+						} else if (array[1].contains("MWTJ2KH")) {
+							array[year] = "2020";
+						} else if (array[1].contains("MVFL2KH")) {
+							array[year] = "2020";
+						} else if (array[1].contains("M1")) {
+							array[year] = "2020";
+						} else if (array[1].contains("MC965LL")) {
+							array[year] = "2012";
+						} else if (array[1].contains("MD711LL")) {
+							array[year] = "2014";
+						} else if (array[1].contains("MJVM2LL")) {
+							array[year] = "2016";
+						} else if (array[1].contains("MD761LL")) {
+							array[year] = "2015";
+						} else if (array[1].contains("MD223LL")) {
+							array[year] = "2013";
+						} else if (array[1].contains("9세대")) {
+							array[year] = "2019";
+						} else if (array[1].contains("I9-9980HK")) {
+							array[year] = "2019";
+						} else if (array[1].contains("I9-9880H")) {
+							array[year] = "2019";
+						} else if (array[1].contains("I7-9750H")) {
+							array[year] = "2020";
+						} else if (array[1].contains("I7-1068NG7")) {
+							array[year] = "2020";
+						} else if (array[1].contains("I5-1038NG7")) {
+							array[year] = "2020";
+						} else if (array[1].contains("I7-8557U")) {
+							array[year] = "2020";
+						} else if (array[1].contains("I5-8257U")) {
+							array[year] = "2020";
+						} else if (array[1].contains("I9-9880H")) {
+							array[year] = "2020";
+						} else if (array[1].contains("I7-9750H")) {
+							array[year] = "2020";
+						} else if (array[1].contains("I7-8557U")) {
+							array[year] = "2019";
+						} else if (array[1].contains("I5-8257U")) {
+							array[year] = "2019";
+						} else if (array[1].contains("I9-9880H")) {
+							array[year] = "2019";
+						} else if (array[1].contains("I7-9750H")) {
+							array[year] = "2019";
+						} else if (array[1].contains("I7-8569U")) {
+							array[year] = "2019";
+						} else if (array[1].contains("I5-8279U")) {
+							array[year] = "2019";
+						} else if (array[1].contains("I5-8") || array[1].contains("I7 8")) {
+							array[year] = "2018";
+						} else if (array[1].contains("I7-8") || array[1].contains("I7 8")) {
+							array[year] = "2018";
+						} else if (array[1].contains("I9-8") || array[1].contains("I7 8")) {
+							array[year] = "2018";
+						} else if (array[1].contains("I7-7") || array[1].contains("I7 7")) {
+							array[year] = "2017";
+						} else if (array[1].contains("I5-7") || array[1].contains("I5 7")) {
+							array[year] = "2017";
+						} else if (array[1].contains("I7-6") || array[1].contains("I7 6")) {
+							array[year] = "2016";
+						} else if (array[1].contains("I5-6") || array[1].contains("I5 6")) {
+							array[year] = "2016";
+						} else if (array[1].contains("I7-4") || array[1].contains("I7 4")) {
+							array[year] = "2014";
+						} else if (array[1].contains("I7-5") || array[1].contains("I7 5")) {
+							array[year] = "2015";
+						} else if (array[1].contains("I5-5") || array[1].contains("I5 5")) {
+							array[year] = "2015";
+						} else if (array[1].contains("I5-4") || array[1].contains("I5 4")) {
+							array[year] = "2014";
+						} else if (array[1].contains("I7-3") || array[1].contains("I7 3")) {
+							array[year] = "2013";
+						} else if (array[1].contains("I5-3") || array[1].contains("I5 3")) {
+							array[year] = "2013";
+						} else if (array[1].contains("I7-2") || array[1].contains("I7 2")) {
+							array[year] = "2011";
+						} else if (array[1].contains("MY")) {
+							array[year] = "2020";
+						} else if (array[1].contains("MW")) {
+							array[year] = "2020";
+						} else if (array[1].contains("MX")) {
+							array[year] = "2020";
+						} else if (array[1].contains("MV")) {
+							array[year] = "2019";
+						} else if (array[1].contains("MU")) {
+							array[year] = "2019";
+						} else if (array[1].contains("MR")) {
+							array[year] = "2018";
+						} else if (array[1].contains("MP")) {
+							array[year] = "2017";
+						} else if (array[1].contains("ML")) {
+							array[year] = "2016";
+						} else if (array[1].contains("MJ")) {
+							array[year] = "2015";
+						} else if (array[1].contains("MF")) {
+							array[year] = "2015";
+						} else if (array[1].contains("MG")) {
+							array[year] = "2014";
+						} else if (array[1].contains("ME")) {
+							array[year] = "2013";
+						} else if (array[1].contains("MD")) {
+							array[year] = "2012";
+						} else if (array[1].contains("MC")) {
+							array[year] = "2012";
+						} else if (array[1].contains("MD223LL")) {
+							array[year] = "2013";
+						} else if (array[1].contains("MD223LL")) {
+							array[year] = "2013";
+						} else if (array[1].contains("MD223LL")) {
+							array[year] = "2013";
+						} else if (array[1].contains("MD223LL")) {
+							array[year] = "2013";
+						} else if (array[1].contains("세대")) {
+							for (int k = 0; k < array[1].length(); k++) {
+								if (array[1].charAt(k) == '세') {
+									int tempval = 2010 + (int) (array[1].charAt(k - 1) - 48);
+									array[year] = String.valueOf(tempval);
+								}
+							}
+						}
+
+					}
+					if (i == 1) { // 다시 원래 값 주입
+						array[1] = temp;
+					}
+					if (i == 2) {
+						brwrite.write(array_join[i]);
+					} else {
+						brwrite.write(array[i]);
+					}
+					if (i != array.length - 1) {
+						brwrite.write(",");
+					}
+				}
+				brwrite.newLine();
+				// 배열에서 리스트 반환
+				tmpList = Arrays.asList(array);
+//				if (array[year] == "empty") {
+//					System.out.println(tmpList);
+//				}
+				ret.add(tmpList);
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null) {
+					br.close();
+					br_join.close();
+					brwrite.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
